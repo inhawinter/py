7b4e971350807aaf43a73f02d1b13360743c9e06
inhawinter <inhawinter@gmail.com>
2023-01-18 21:58:45 +0900

    day03 list01

--- day03.py
+++ day03.py
@@ -1,16 +1,27 @@
-# prime number v0.9
+# list
 
-number_list = input('input 2 integer number : ').split()
-start = int(number_list[0])
-end = int(number_list[1])
+bigbang = ['지디', '태양', '탑', '대성', '승리']
+exo = ['백현', '첸']
+# exo.append('시우민')
+exo.insert(1, '시우민')
+print(exo)
+# exo.extend(bigbang)
+# exo = exo + bigbang
 
-if end < start:
-    start, end = end, start  # tuple packing & unpacking
-
-for number in range(start, end+1):
-    if number > 1:
-        for k in range(2, number):
-            if number % k == 0:
-                break
-        else:
-            print(number, end=' ')
+exo.append(bigbang)
+print(exo)
+# 태양
+print(exo[-1][1])
+print(exo[3][1])
+print(exo[3][-4])
+print(exo[3].pop())  # 승리 return 후 pop
+print(exo)
+print(exo[3].pop(2))  # 탑 return 후 pop
+print(exo)
+# exo.remove('대성')  # exo 리스트에서 대성을 찾을 수 없음
+exo[3].remove('대성')  # 대성 remove
+print(exo)
+del exo[2]  # 첸 delete
+print(exo)
+exo.clear()
+print(exo)
