9754ff938e0e03c81380ed81bb12ddeea4ee9eae
inhawinter <inhawinter@gmail.com>
2023-01-18 20:06:38 +0900

    day03 prime number v0.4

--- day03.py
+++ day03.py
@@ -1,14 +1,13 @@
-# prime number v0.3
+# prime number v0.4
 
 number = int(input('input integer number : '))
 is_prime = True  # True가 유지 되면 소수
 
-for k in range(1, number+1):  # 2부터 입력 된 수 앞까지 반복
+for k in range(2, number):  # 2부터 입력 된 수 앞까지 반복
     if number % k == 0:  # k로 나누어 떨어지면
-        # counts = counts + 1
         is_prime = False
+        break  # 첫 번째 약수 발견시 for문 탈출
 
-#if is_prime == True:  # is_prime 변수의 True 값이 유지되면 소수
 if is_prime:  # is_prime 변수의 True 값이 유지되면 소수
     print(f'{number} is prime number!')
 else:
