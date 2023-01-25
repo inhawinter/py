f9f874b7f9f7e8f6c2d0a4bf1cd7b507412ae31f
inhawinter <inhawinter@gmail.com>
2023-01-25 21:52:22 +0900

    day07 function09

--- day07.py
+++ day07.py
@@ -1,22 +1,27 @@
-def test(start=0, end=5):
-    n = start
-    #for i in range(start, end+1):
-    while n <= end:
-        yield n
-        n = n + 1
+def document_info(func):  # decorator
+    def new_function(*args, **kwargs):
+        print('실행 중인 함수:', func.__name__)
+        print('위치 인수들', args)
+        print('키워드 인수들', kwargs)
+        result = func(*args, **kwargs)
+        print('실행결과:', result)
+        return result
+    return new_function
 
-print(test)
-g = test(5, 10)
-# g = test()
-print(g)
-for k in g:
-    print(k)
 
-for k in g:
-    print(k)
+def squares(n):
+    return n * n
 
-#a = (p for p in zip([1, 2, 3],[4, 5 ,6],[7, 8, 9, 10]))
-a = (p for p in zip({(1, 1): 3.99, (1, 2): 4.31}, {(2, 1): 4.19, (2, 2): 4.01}))
-print(type(a), a)
-for i in a:
-    print(i)
+
+def subtract(a, b):
+    return a - b
+
+
+info_squares = document_info(squares)
+r1 = info_squares(9)
+print(r1)
+# print(squares(7))
+print('==================')
+info_minus = document_info(subtract)
+r2 = info_minus(99, 7)
+print(r2)
