dff36c929ddac4c39bb6bf58f6ad47cbc3bac052
inhawinter <inhawinter@gmail.com>
2023-01-25 22:05:28 +0900

    day07 function11

--- day07.py
+++ day07.py
@@ -1,24 +1,10 @@
-def document_info(func):  # decorator
-    def new_function(*args, **kwargs):
-        print('실행 중인 함수:', func.__name__)
-        print('위치 인수들', args)
-        print('키워드 인수들', kwargs)
-        result = func(*args, **kwargs)
-        print('실행결과:', result)
-        return result
-    return new_function
+def a():
+    def b():
+        return "안쪽 함수 실행"
+    return b
 
-
-@document_info
-def squares(n):
-    return n * n
-
-
-@document_info
-def subtract(a, b):
-    return a - b
-
-
-print(squares(5))
-print(subtract(5, 99))
+print(a())
+c = a()
+print(c())
+# print(b())  # a함수에서만 볼 수 있다.
 
