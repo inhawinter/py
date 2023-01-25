bce731338403610407d68171a9e802f23e147397
inhawinter <inhawinter@gmail.com>
2023-01-25 21:42:56 +0900

    day07 function08

--- day07.py
+++ day07.py
@@ -1,20 +1,22 @@
-def a():  # generator
-    yield 1
-    yield -9
-    yield 3
+def test(start=0, end=5):
+    n = start
+    #for i in range(start, end+1):
+    while n <= end:
+        yield n
+        n = n + 1
 
+print(test)
+g = test(5, 10)
+# g = test()
+print(g)
+for k in g:
+    print(k)
 
-def b():  # normal function
-    return 1  # stop
-    return -9
-    return 3
+for k in g:
+    print(k)
 
-print(type(a()), b())
-c = a()
-print(c)
-
-for i in c:
-    print(i)
-
-for i in c:
+#a = (p for p in zip([1, 2, 3],[4, 5 ,6],[7, 8, 9, 10]))
+a = (p for p in zip({(1, 1): 3.99, (1, 2): 4.31}, {(2, 1): 4.19, (2, 2): 4.01}))
+print(type(a), a)
+for i in a:
     print(i)
