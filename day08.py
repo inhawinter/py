62b98281b23d653d0030238918c463fa102705db
inhawinter <inhawinter@gmail.com>
2023-01-26 20:52:54 +0900

    day08 class03

--- day08.py
+++ day08.py
@@ -1,11 +1,10 @@
 # class & object
 
 class Pokemon:
-    def __init__(self, name, owner, skills):  # constructor
-        self.name = name  # attribute, field, member variable
-        self.owner = owner
+    def __init__(self, owner, skills):  # constructor
+        self.owner = owner  # attribute, field, member variable
         self.skills = skills.split('/')  # list
-        print(f'포켓몬 {name} 생성됨')
+        print(f'포켓몬 생성됨 :', end=' ')
 
     def info(self):  # method (member function)
         """
@@ -18,11 +17,33 @@ class Pokemon:
         print('===========')
 
 
+    def attack(self, idx):
+        print(f'{self.skills[idx]} 공격 시전!')
+
+
+
 class Pikachu(Pokemon):  # class 자식클래스(부모클래스):
-    pass
+    def __init__(self, owner, skills):
+        super().__init__(owner, skills)
+        self.name = "피카츄"
+        print(f'{self.name}')
+
+    def attack(self, idx):  # override
+        print(f'[삐까삐까] {self.owner}의 {self.name}가 {self.skills[idx]} 공격 시전!')
 
 
-pika1 = Pikachu('피카츄', '한지우', '번개/50만 볼트/100만 볼트/전광석화')
-pika1.info()
-p1 = Pokemon('꼬부기', '오바람', '거품/몸통박치기')
-p1.info()
+class Ggoboogi(Pokemon):  # inheritance
+    def __init__(self, owner, skills):
+        super().__init__(owner, skills)
+        self.name = "꼬부기"
+        print(f'{self.name}')
+
+    def attack(self, idx):  # override
+        print(f'[꼬북꼬북] {self.owner}의 {self.name}가 {self.skills[idx]} 공격 시전!')
+
+pika1 = Pikachu('한지우', '번개/100만 볼트')
+ggo1 = Ggoboogi('오바람', '거품/물대포/몸통박치기')
+# print(ggo1.owner)
+# pika1.info()
+pika1.attack(1)
+ggo1.attack(2)
