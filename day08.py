0e1e94de1e8126db00d358ee86bf6bc71d0704a6
inhawinter <inhawinter@gmail.com>
2023-01-26 21:50:04 +0900

    day08 class04

--- day08.py
+++ day08.py
@@ -2,16 +2,23 @@
 
 class Pokemon:
     def __init__(self, owner, skills):  # constructor
-        self.owner = owner  # attribute, field, member variable
+        self.hidden_owner = owner  # attribute, field, member variable
         self.skills = skills.split('/')  # list
-        print(f'포켓몬 생성됨 :', end=' ')
+        import datetime  # 상속관계 x
+        print(f'[{datetime.datetime.now()}] 포켓몬 생성됨 :', end=' ')
+
+    def get_owner(self):
+        return self.hidden_owner
+
+    def set_owner(self, owner):
+        self.hidden_owner = owner
 
     def info(self):  # method (member function)
         """
         포켓몬 정보(주인, 이름, 기술들) 출력
         :return: void
         """
-        print(f'{self.owner}의 포켓몬은 {self.name}입니다')
+        print(f'{self.hidden_owner}의 포켓몬은 {self.name}입니다')
         for skill in self.skills:
             print(f'===========\n{skill}')
         print('===========')
@@ -20,7 +27,7 @@ class Pokemon:
     def attack(self, idx):
         print(f'{self.skills[idx]} 공격 시전!')
 
-
+    owner = property(get_owner, set_owner)
 
 class Pikachu(Pokemon):  # class 자식클래스(부모클래스):
     def __init__(self, owner, skills):
@@ -29,7 +36,7 @@ class Pikachu(Pokemon):  # class 자식클래스(부모클래스):
         print(f'{self.name}')
 
     def attack(self, idx):  # override
-        print(f'[삐까삐까] {self.owner}의 {self.name}가 {self.skills[idx]} 공격 시전!')
+        print(f'[삐까삐까] {self.hidden_owner}의 {self.name}가 {self.skills[idx]} 공격 시전!')
 
 
 class Ggoboogi(Pokemon):  # inheritance
@@ -39,7 +46,7 @@ class Ggoboogi(Pokemon):  # inheritance
         print(f'{self.name}')
 
     def attack(self, idx):  # override
-        print(f'[꼬북꼬북] {self.owner}의 {self.name}가 {self.skills[idx]} 공격 시전!')
+        print(f'[꼬북꼬북] {self.hidden_owner}의 {self.name}가 {self.skills[idx]} 공격 시전!')
 
 pika1 = Pikachu('한지우', '번개/100만 볼트')
 ggo1 = Ggoboogi('오바람', '거품/물대포/몸통박치기')
@@ -47,3 +54,13 @@ ggo1 = Ggoboogi('오바람', '거품/물대포/몸통박치기')
 # pika1.info()
 pika1.attack(1)
 ggo1.attack(2)
+
+# #print(pika1.hidden_owner)  # public, 직접적인 접근 가능
+# print(pika1.get_owner())  # getter 접근 가능
+# pika1.set_owner('덴트')  # setter로 변경
+# # pika1.hidden_owner = '덴트'
+# print(pika1.get_owner())  # getter 접근 가능
+
+print(pika1.owner)  # property 이용
+pika1.owner = '덴트'  # property 이용
+print(pika1.owner)  # property 이용
