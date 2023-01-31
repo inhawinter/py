6cfd3b34280cec8d41b63b27ba0ee7c3d3cc5e48
inhawinter <inhawinter@gmail.com>
2023-01-31 20:46:03 +0900

    day10 web 04

--- day10.py
+++ day10.py
@@ -6,21 +6,41 @@ html = """
 <title>스크레이핑 실습</title>
 </head>
 <body>
-<h1>인하대학교</h1>
-<p>웹스크레이핑</p>
-<p>파이썬 기본 문법, 판다스, GUI ... </p>
+<a href="http://www.inha.ac.kr">인하대학교</a><br>
+<a href="http://www.harvard.edu">하버드대학교</a>
 </body>
 </html>
 """
-
+# 인하대학교의 url주소는 http://www.inha.ac.kr입니다.
 soup = BeautifulSoup(html, 'html.parser')
-t = soup.html.head.title
-h1 = soup.html.body.h1.string
-p1 = soup.html.body.p
-p2 = p1.next_sibling.next_sibling
+urls = soup.find_all("a")  # return list
+# print(urls)
+for url in urls:
+    print('{0}의 url주소는 {1}입니다.'.format(url.string, url.attrs['href']))
+    # univ = url.string
+    # link = url.attrs['href']
+    # print('{0}의 url주소는 {1}입니다.'.format(univ, link))
 
-print(p1.string)
-print(p2.string)
-print(h1)
-print(t)
-print(t.string)
+
+
+# from bs4 import BeautifulSoup
+#
+# html = """
+# <html>
+# <head>
+# <title>스크레이핑 실습</title>
+# </head>
+# <body>
+# <h1 id="univ">인하대학교</h1>
+# <p>웹스크레이핑</p>
+# <p id="contents">파이썬 기본 문법, 판다스, GUI ... </p>
+# </body>
+# </html>
+# """
+#
+# soup = BeautifulSoup(html, 'html.parser')
+# university = soup.find(id='univ')
+# contents = soup.find(id='contents')
+#
+# print(contents.string)
+# print(university.string)
