267acbcaa915c9465326d5417a5cef9f8ffa185b
inhawinter <inhawinter@gmail.com>
2023-01-31 21:34:31 +0900

    day10 web 05

--- day10.py
+++ day10.py
@@ -1,46 +1,27 @@
+import urllib.request
 from bs4 import BeautifulSoup
 
-html = """
-<html>
-<head>
-<title>스크레이핑 실습</title>
-</head>
-<body>
-<a href="http://www.inha.ac.kr">인하대학교</a><br>
-<a href="http://www.harvard.edu">하버드대학교</a>
-</body>
-</html>
-"""
-# 인하대학교의 url주소는 http://www.inha.ac.kr입니다.
-soup = BeautifulSoup(html, 'html.parser')
-urls = soup.find_all("a")  # return list
-# print(urls)
-for url in urls:
-    print('{0}의 url주소는 {1}입니다.'.format(url.string, url.attrs['href']))
-    # univ = url.string
-    # link = url.attrs['href']
-    # print('{0}의 url주소는 {1}입니다.'.format(univ, link))
+api = 'https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp'
+urls = urllib.request.urlopen(api).read()
+soup = BeautifulSoup(urls, "html.parser")
+
+cities = soup.find_all("city")
+datas = soup.find_all("data")
+
+for i in range(len(cities)):
+    print(f'{cities[i].string}의 날씨는 {datas[i*13].find("wf").string}입니다.')
 
 
-
+# import urllib.request
 # from bs4 import BeautifulSoup
 #
-# html = """
-# <html>
-# <head>
-# <title>스크레이핑 실습</title>
-# </head>
-# <body>
-# <h1 id="univ">인하대학교</h1>
-# <p>웹스크레이핑</p>
-# <p id="contents">파이썬 기본 문법, 판다스, GUI ... </p>
-# </body>
-# </html>
-# """
+# api = 'https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp'
+# urls = urllib.request.urlopen(api).read()
+# soup = BeautifulSoup(urls, "html.parser")
 #
-# soup = BeautifulSoup(html, 'html.parser')
-# university = soup.find(id='univ')
-# contents = soup.find(id='contents')
+# cities = soup.find_all("city")
+# wfs = soup.find_all("wf")
+# wfs.pop(0)  # 맨 앞의 wf태그 내용은 중기 날씨 요약 뉴스
 #
-# print(contents.string)
-# print(university.string)
+# for i in range(len(cities)):
+#     print(f'{cities[i].string}의 날씨는 {wfs[i*13].string}입니다.')
